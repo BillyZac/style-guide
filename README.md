b874d38cddc1200f06a98ce3beff1957a2441c7b
Zachary Smith <william.smith1@wipro.com>
2016-10-06 11:55:34 -0400

    Add README with usage instructions. Elaborate the Guide

---
+++ README.md
@@ -0,0 +1,19 @@
+# Style guide
+
+## Usage
+Install dependencies:
+```
+npm install
+```
+
+Build:
+```
+npm run build
+```
+
+And start the dev server:
+```
+npm start
+```
+
+Visit the guide at http://localhost:3333/
--- main.js
+++ main.js
@@ -7,6 +7,11 @@ class Thing extends React.Component {
   render() {
     return (
       <div>
+        <h1>Style guide</h1>
+        <p>This is a Hey component.</p>
+        <p>To make one for yourself import the component and style, like so:</p>
+        <pre>const Hey = require('whippersnapper/lib/react/Hey.js')</pre>
+        <pre>require('zzzss/dist/css/zzzss.css')</pre>
         <Hey />
       </div>
     )
