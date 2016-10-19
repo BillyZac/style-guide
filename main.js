a4caa4ce7dc18146207e394264350eeeb9a69d0e
Zachary Smith <william.smith1@wipro.com>
2016-10-19 12:25:33 -0400

    Add code snippet. Style the style guide.

--- main.js
+++ main.js
@@ -2,19 +2,52 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 const Hey = require('whippersnapper/build/Hey.js')
 const Text = require('whippersnapper/build/Text.js')
+require('./style.css')
 require('zzzss/dist/css/zzzss.css')
+
+
+const Code = React.createClass({
+  getInitialState: function() {
+      return { isCodeSnippetVisible: false }
+  },
+
+  render: function() {
+    const { codeSnippet } = this.props;
+    const codeSnippetClass =
+      this.state.isCodeSnippetVisible ? `code-snippet` : `code-snippet hidden`
+    return (
+      <div>
+        <span
+          className="code"
+          onClick={ () => {
+            this.setState({ isCodeSnippetVisible: !this.state.isCodeSnippetVisible })
+          }}
+          >code</span>
+        <div
+            className={codeSnippetClass}
+          >{codeSnippet}</div>
+        </div>
+    )
+  }
+})
 
 class Thing extends React.Component {
   render() {
     return (
       <div>
-        <h1>Style guide</h1>
-        <h2>This is a Hey component.</h2>
+        <div className="intro">
+          <h1>Style guide</h1>
+          <p>Demonstrating the Whippersnapper React component library with the zzzss style library.</p>
+        </div>
+        <h2>Hey component</h2>
         <Hey />
+        <Code codeSnippet="<Hey />" />
+        <h2>Text component</h2>
         <Text
           label="Planet"
           content="Jupiter"
         />
+        <Code codeSnippet='<Text label="Planet" content="Jupiter" />' />
       </div>
     )
   }
