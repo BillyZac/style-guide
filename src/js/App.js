9f0eff262e5214e163d4df800e6b2be6db45f2c5
Zachary Smith <william.smith1@wipro.com>
2016-12-13 16:35:04 -0700

    Begin to modularize

--- src/js/App.js
+++ src/js/App.js
@@ -1,35 +1,11 @@
 import React from 'react'
+import Code from './components/Code.js'
 
 const Text = require('whippersnapper/build/Text.js')
 import Header from 'whippersnapper/build/Header';
 const Footer = require('whippersnapper/build/Footer.js')
 require('zzzss/dist/css/zzzss.css')
-require('./style.css')
-
-const Code = React.createClass({
-  getInitialState: function() {
-      return { isCodeSnippetVisible: false }
-  },
-
-  render: function() {
-    const { codeSnippet } = this.props;
-    const codeSnippetClass =
-      this.state.isCodeSnippetVisible ? `code-snippet` : `code-snippet hidden`
-    return (
-      <div>
-        <span
-          className="code"
-          onClick={ () => {
-            this.setState({ isCodeSnippetVisible: !this.state.isCodeSnippetVisible })
-          }}
-          >code</span>
-        <div
-            className={codeSnippetClass}
-          >{codeSnippet}</div>
-        </div>
-    )
-  }
-})
+require('../style.css')
 
 class App extends React.Component {
   render() {
@@ -42,15 +18,15 @@ class App extends React.Component {
         <div className="intro">
           <p>Demonstrating the Whippersnapper React component library with the zzzss style library.</p>
         </div>
+
         <h2>Text component</h2>
         <Text
           label="Planet"
           content="Jupiter"
         />
         <Code codeSnippet='<Text label="Planet" content="Jupiter" />' />
-        <Footer
-          appVersion="1.0"
-        />
+
+        <Footer appVersion="1.0" />
       </div>
     )
   }
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const buildFolder = 'build'
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/js/main.js',
   output: {
     path: path.resolve(__dirname, buildFolder),
     filename: 'index.js',
