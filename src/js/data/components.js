099f0f92ae5d5a915d6641de7a10cce15348a960
Zachary Smith <william.smith1@wipro.com>
2016-12-14 16:20:35 -0700

    Add Button

--- src/js/data/components.js
+++ src/js/data/components.js
@@ -1,11 +1,42 @@
 import React from 'react'
 const Text = require('whippersnapper/build/Text.js')
+import Button from 'whippersnapper/build/Button.js'
 
 const components = [
   {
     title: "Text component",
     component: <Text label="Planet" content="Jupiter" />,
     codeSnippet: '<Text label="Planet" content="Jupiter" />',
+  },
+  {
+    title: "Button component",
+    component: (
+      <Button
+        label="Click me"
+        onClick={() => { console.log('clicked!') }}
+        cssClasses="normal"
+      />
+    ),
+    codeSnippet: `<Button
+      label="Click me"
+      onClick={() => { console.log('clicked!') }}
+      cssClasses="normal"
+    />`,
+  },
+  {
+    title: "Warning Button",
+    component: (
+      <Button
+        label="Watch out!"
+        onClick={() => { console.log('clicked!') }}
+        cssClasses="warning"
+      />
+    ),
+    codeSnippet: `<Button
+      label="Click me"
+      onClick={() => { console.log('clicked!') }}
+      cssClasses="warning"
+    />`,
   },
 ]
 
